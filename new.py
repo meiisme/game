f9fa5a8af4b83cc504f9c9f106bbaa9c451dc84d
meiisme <95857607+meiisme@users.noreply.github.com>
2021-12-23 20:35:34 +0800

    Update new.py

--- new.py
+++ new.py
@@ -9,8 +9,9 @@ a_count=0 # initial A count
 b_count=0 # initial B count
 for i in range(4):
     answer+=str(items[i])
-while(True):
     random.shuffle(items)
+while(True):
+    
     number=st.text_input('請輸入數字： ')
     st.write(answer)
     if not number.isdigit():  #cheak all input is digit
