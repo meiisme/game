71c7ab2be5669d008a2cd19c2f57e92035779cc6
meiisme <95857607+meiisme@users.noreply.github.com>
2021-12-23 16:34:28 +0800

    Update AABB.py

--- AABB.py
+++ AABB.py
@@ -5,7 +5,7 @@ answer = random.sample(range(1, 10), 4)
 a = b = n = 0     
 while a!=4:       
   a = b = n = 0   
-  user = st.number_input(label='輸入四個數字：') 
+  user = st.text_input(label='輸入四個數字：') 
   submit_button = st.button(label='確定')                 
 if (user[n]) == answer[n]:   
       a += 1                         
