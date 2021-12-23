db3ec847ff64171145223abba9ab280af03f904c
meiisme <95857607+meiisme@users.noreply.github.com>
2021-12-23 21:39:19 +0800

    Update new.py

--- new.py
+++ new.py
@@ -7,10 +7,9 @@ import streamlit as st
 
 from gamestate import persistent_game_state
 
-import random
-
 items = ['1','2','3','4','5','6','7','8','9','0']
-answer=random.shuffle(items)
+
+
 answer=''
 a_count=0 # initial A count
 b_count=0 # initial B count
@@ -26,12 +25,12 @@ while(True):
         if number==answer:
             st.write('很棒你答對了')
             break
-        for i in range(4):
-            for j in range(4):
-                if i==j and number[i]==answer[j]:
-                    a_count+=1
-                elif number[i]==answer[j]:
-                    b_count+=1
+    for i in range(4):
+        for j in range(4):
+            if i==j and number[i]==answer[j]:
+                a_count+=1
+            elif number[i]==answer[j]:
+                b_count+=1
         st.write('{0}A{1}B'.format(a_count,b_count))
         a_count=0
         b_count=0
