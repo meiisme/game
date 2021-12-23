34e434a4bf219a6637ffbafdc7afb3d921a9ff64
meiisme <95857607+meiisme@users.noreply.github.com>
2021-12-23 20:04:25 +0800

    Update new AABB.py

--- new AABB.py
+++ new AABB.py
@@ -1,10 +1,10 @@
 #1A2B game
 import streamlit as st
 import random
-
+answer = random.sample(range(1, 10), 4)
 
 items = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
-random.shuffle(items)
+
 answer=''
 a_count=0 # initial A count
 b_count=0 # initial B count
