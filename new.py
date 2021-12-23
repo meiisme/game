5fe4913713a243c8fcc6eb14186456bc26469e33
meiisme <95857607+meiisme@users.noreply.github.com>
2021-12-23 20:57:41 +0800

    Update new.py

--- new.py
+++ new.py
@@ -1,9 +1,9 @@
 #1A2B game
 import streamlit as st
 import random
-random.shuffle(items)
-items = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
 
+items = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+random.shuffle(items)
 answer=''
 a_count=0 # initial A count
 b_count=0 # initial B count
