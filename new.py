5d1c9dbd146f30d69785947b44272da50eceb65a
meiisme <95857607+meiisme@users.noreply.github.com>
2021-12-30 20:22:36 +0800

    Update new.py

--- new.py
+++ new.py
@@ -1,11 +1,13 @@
 #1A2B game
 import random
 import streamlit as st
+
 items = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
 random.shuffle(items)
-st.session_state(answer)
 st.write(answer)
+st.session_state(answer)
 answer=''
+
 a_count=0 # initial A count
 b_count=0 # initial B count
 for i in range(4):
