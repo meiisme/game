56976662f3219601d696987c58f0406588acbb02
meiisme <95857607+meiisme@users.noreply.github.com>
2021-12-30 20:21:00 +0800

    Update new.py

--- new.py
+++ new.py
@@ -3,6 +3,7 @@ import random
 import streamlit as st
 items = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
 random.shuffle(items)
+st.session_state(answer)
 answer=''
 a_count=0 # initial A count
 b_count=0 # initial B count
