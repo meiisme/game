86433a13aa690acc04019ca5c87fc23794a5361f
meiisme <95857607+meiisme@users.noreply.github.com>
2022-01-06 19:47:50 +0800

    Update new.py

--- new.py
+++ new.py
@@ -28,24 +28,24 @@ if submit_button:
         answer+=str(items[i])
     st.session_state.answer=answer
 
-    number=st.sidebar.text_input('請輸入數字')
+number=st.sidebar.text_input('請輸入數字')
 #while(True):
     #number=st.text_input('Enter the number: ')
 #st.write(st.session_state.answer)
-    if not number.isdigit():  #cheak all input is digit
-        pass
-    else:
-        if number==st.session_state.answer:
-            st.write('好棒棒！你猜對了')
-            #break
-        for i in range(4):
-            #st.session_state(st.session_state.answer)
-            for j in range(4):
-                if i==j and number[i]==st.session_state.answer[j]:
-                    a_count+=1
-                elif number[i]==st.session_state.answer[j]:
-                    b_count+=1
+if not number.isdigit():  #cheak all input is digit
+    pass
+else:
+    if number==st.session_state.answer:
+        st.write('好棒棒！你猜對了')
+        #break
+    for i in range(4):
+        #st.session_state(st.session_state.answer)
+        for j in range(4):
+            if i==j and number[i]==st.session_state.answer[j]:
+                a_count+=1
+            elif number[i]==st.session_state.answer[j]:
+                b_count+=1
                 #    st.session_state(answer)
-        st.write(a_count, 'A', b_count, 'B')
-        a_count=0
-        b_count=0
+    st.write(a_count, 'A', b_count, 'B')
+    a_count=0
+    b_count=0
