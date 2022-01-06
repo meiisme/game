45153bcd6e0b3aec036195667412057ebf86686d
meiisme <95857607+meiisme@users.noreply.github.com>
2022-01-06 20:03:23 +0800

    Update new.py

--- new.py
+++ new.py
@@ -22,13 +22,14 @@ a_count=0 # initial A count
 b_count=0 # initial B count
 
 submit_button = st.button(label='開始遊戲')   
+number=st.sidebar.text_input('請輸入數字')
+st.write(st.session_state.answer)
+
 if submit_button:
     for i in range(4):
         answer+=str(items[i])
     st.session_state.answer=answer
 
-number=st.sidebar.text_input('請輸入數字')
-st.write(st.session_state.answer)
 
 #while(True):
     #number=st.text_input('Enter the number: ')
